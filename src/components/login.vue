b9ad60f215041b5be06f878e1766581399872286
zhj <1528863673@qq.com>
2019-02-14 19:29:58 +0800

    渲染home

--- src/components/login.vue
+++ src/components/login.vue
@@ -10,7 +10,9 @@
     <el-form-item label="密码">
         <el-input v-model="formdata.password"></el-input>
     </el-form-item>
-     <el-button class="login-btn" type="success">登录</el-button>
+     <el-button 
+     @click.prevent="handleLogin()"
+     class="login-btn" type="success">登录</el-button>
   </el-form>
 </div>
 </template>
@@ -25,10 +27,56 @@ export default {
 
             }
         }
-    }
+    },
+    methods: {
+        //登录请求
+        handleLogin(){
+            console.log(1);
+            this.$http
+                .post(`login`,this.formdata)
+                .then((res)=>{
+                    console.log(2)
+                    console.log(res)
+                    const {
+                        data: {
+                            data,
+                            meta: { msg, status }
+                        }
+                    } = res;
+                    if (status === 200) {
+                        // console.log("sucss----");
+                        this.$router.push({
+                            name:'home'
+                        })
+                    } else {
+                         this.$message.error(msg);
+                         console.log("err")
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+    },
 }
 </script>
 
 <style>
-
+  .login-wrap{
+      /* 注意：百分比布局 父元素height */
+      height: 100%;
+      background-color:#324152;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+  } 
+  .login-form {
+      background-color: #fff;
+      border-radius: 5px;
+      width: 400px;
+      padding: 30px;
+  } 
+  .login-btn {
+      width:100%;
+  }
 </style>
