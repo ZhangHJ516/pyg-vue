9fe245550a3ee98441c734adaf3816e270a07820
ZhangHJ516 <1528863673@qq.com>
2019-02-20 16:45:12 +0800

    调整笔记

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,40 +1,45 @@
 <template>
-    <el-container class="container">
-  <el-header>
+  <el-container class="container">
+    <el-header>
       <el-row>
-            <el-col :span="4">
-               <img src="@/assets/logo.png" alt="图片加载失败">
-            </el-col>
-            <el-col :span="19" class="middle">
-               <h2>电商管理系统</h2>
-            </el-col>
-            <el-col :span="1">
-                <a href="#" class="logout" @click="handleLoginout()">退出</a>
-            </el-col>
+        <el-col :span="4">
+          <img src="@/assets/logo.png" alt="图片加载失败">
+        </el-col>
+        <el-col :span="19" class="middle">
+          <h2>电商管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <a href="#" class="logout" @click="handleLoginout()">退出</a>
+        </el-col>
       </el-row>
-  </el-header>
-  <el-container>
-    <el-aside class="aside" width="200px"> 
-      <el-menu
-      :unique-opened="true"
-      :router="true"
-      default-active="2"
-      class="el-menu-vertical-demo">
-      <!-- 1 -->
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>用户管理</span>
-        </template>
-      
-          <el-menu-item index="users">
-               <i class="el-icon-menu"></i>
-              <span>用户列表</span>
-              </el-menu-item>
-      </el-submenu>
+    </el-header>
+    <el-container>
+      <el-aside class="aside" width="200px">
+        <el-menu
+          :unique-opened="true"
+          :router="true"
+          default-active="2"
+          class="el-menu-vertical-demo"
+        >
+          <!-- 1 -->
+          <el-submenu :index="item1.order+''" v-for="(item1,i) in menus" :key="item1.id">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>{{item1.authName}}</span>
+            </template>
 
-    <!-- 2 -->
-      <el-submenu index="2">
+            <el-menu-item
+              :index="item2.path+''"
+              v-for="(item2,i) in item1.children"
+              :key="item2.id"
+            >
+              <i class="el-icon-menu"></i>
+              <span>{{item2.authName}}</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <!-- 2 -->
+          <!-- <el-submenu index="2">
         <template slot="title">
           <i class="el-icon-location"></i>
           <span>权限管理</span>
@@ -48,9 +53,9 @@
                <i class="el-icon-menu"></i>
             <span>权限列表</span>
           </el-menu-item>
-      </el-submenu>
-      <!-- 3 -->
-        <el-submenu index="3">
+          </el-submenu>-->
+          <!-- 3 -->
+          <!-- <el-submenu index="3">
         <template slot="title">
           <i class="el-icon-location"></i>
           <span>商品管理</span>
@@ -69,9 +74,9 @@
               商品分类
           </el-menu-item>
 
-      </el-submenu>
-      <!-- 4 -->
-        <el-submenu index="4">
+          </el-submenu>-->
+          <!-- 4 -->
+          <!-- <el-submenu index="4">
         <template slot="title">
           <i class="el-icon-location"></i>
           <span>订单管理</span>
@@ -81,9 +86,9 @@
                <i class="el-icon-menu"></i>
              订单列表
               </el-menu-item>
-      </el-submenu>
-      <!-- 5 -->
-        <el-submenu index="5">
+          </el-submenu>-->
+          <!-- 5 -->
+          <!-- <el-submenu index="5">
         <template slot="title">
           <i class="el-icon-location"></i>
           <span>数据统计</span>
@@ -93,66 +98,84 @@
             <i class="el-icon-menu"></i>
             数据报表
             </el-menu-item>
-      </el-submenu>
-
-    </el-menu>
-    </el-aside>
-    <el-main class="main">
+          </el-submenu>-->
+        </el-menu>
+      </el-aside>
+      <el-main class="main">
         <router-view></router-view>
-    </el-main>
-  </el-container>
+      </el-main>
+    </el-container>
   </el-container>
 </template>
 
 <script>
 export default {
-    //页面加载之前
-    beforeMount() {
-        if(!localStorage.getItem("token")) {
-            this.$router.push({
-                name:"login"
-            });
-        }
-        this.$message.warning("请先登录")
-    },
-    mounted() {
-        console.log(111)
-    },
-    methods:{
-        //退出
-        handleLoginout(){
-            //1.清除token
-        localStorage.clear();
-            //2.来到登录
-        this.$router.push({
-            name:'login'
-        })
-            //3.提示
-        this.$message.warning("退出成功")
-        }
-    }
-};
+  data() {
+    return {
+      menus: []
+    };
+  },
+  //页面加载之前
+  beforeMount() {
+  
+  },
+  mounted() {
+    console.log(111);
+  },
+  created() {
+    this.getMenus();
+  },
+  methods: {
+    //动态导航栏
+    // 动态导航
+    async getMenus() {
+      // admin登录 -> 保存了后台返回的admin的主管角色的token
+      // laowang38登录->保存了后台返回的laowang38的超级管理员角色的token
 
+      const res = await this.$http.get(`menus`);
+      console.log(res);
+      const {
+        meta: { msg, status },
+        data
+      } = res.data;
+      if (status === 200) {
+        this.menus = data;
+      }
+    },
+
+    //退出
+    handleLoginout() {
+      //1.清除token
+      localStorage.clear();
+      //2.来到登录
+      this.$router.push({
+        name: "login"
+      });
+      //3.提示
+      this.$message.warning("退出成功");
+    }
+  }
+};
 </script>
 
 <style>
 .container {
-    height:100%;
-    background-color:#b3c0D1;
+  height: 100%;
+  background-color: #b3c0d1;
 }
-.aside{
-    background-color: yellow;
+.aside {
+  background-color: yellow;
 }
-.main{
-    /* background-color:green; */
+.main {
+  /* background-color:green; */
 }
-.middle{
-    line-height: 60px;
-    text-align: center;
-    color:#fff;
+.middle {
+  line-height: 60px;
+  text-align: center;
+  color: #fff;
 }
-.logout{
-    line-height:60px;
-    text-decoration: none;
+.logout {
+  line-height: 60px;
+  text-decoration: none;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import {
+  Message
+} from 'element-ui';
+
 import Login from '@/components/login.vue'
 import Home from '@/components/home.vue'
 import Users from '@/components/users.vue'
@@ -7,14 +11,14 @@ import Rights from '@/components/rights.vue'
 import Roles from '@/components/roles.vue'
 
 Vue.use(Router)
-
-export default new Router({
+//实例化路由
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home,
-      children:[{
+      children: [{
         name: 'users',
         path: '/users',
         component: Users
@@ -29,7 +33,7 @@ export default new Router({
         path: 'roles',
         component: Roles
       }
-    ]
+      ]
     }, {
       name: 'login',
       path: '/login',
@@ -37,3 +41,50 @@ export default new Router({
     }
   ]
 })
+
+
+
+// 路由导航守卫(保安)
+// 任何路由标识变化->来到路由配置->自动来到路由守卫->渲染组件
+
+// to->要去的路由配置对象 (name)
+// from->当前的路由配置对象 (name)
+// next -> 方法 next()->让路由配置继续生效
+router.beforeEach((to, from, next) => {
+  // 如果要去的是login -> next()
+  if (to.name === 'login') {
+    next();
+  } else {
+    // 如果要去的不是login ->
+    //  2.1 !token -> 去登录
+    const token = localStorage.getItem("token");
+    if (!token) {
+      //提示
+      // this.$message.warning("请先登录!");->
+      Message.warning("请先登录!");
+
+      //  this.$router.push({name:'login'})
+      // $router
+      router.push({
+        name: 'login'
+      })
+      return;
+    }
+    //  2.2 token  -> next()
+    next();
+
+
+
+  }
+
+
+  // console.log(to, from);
+
+  // next方法
+
+
+
+})
+
+export default router;
+
